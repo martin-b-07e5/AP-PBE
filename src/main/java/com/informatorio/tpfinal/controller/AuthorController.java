61f267a7bee4abee1697e0e86bdaff7326b394da
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-09 12:01:56 -0300

    .

--- src/main/java/com/informatorio/tpfinal/controller/AuthorController.java
+++ src/main/java/com/informatorio/tpfinal/controller/AuthorController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 //https://www.baeldung.com/spring-controller-vs-restcontroller
 //https://www.baeldung.com/spring-requestmapping
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/author")
 public class AuthorController {
 }
