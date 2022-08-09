df03a68e940aa8a8aa8e9f157707f5f557ba3229
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-09 10:32:55 -0300

    added anotation @EntityScan

--- src/main/java/com/informatorio/tpfinal/NewsApp.java
+++ src/main/java/com/informatorio/tpfinal/NewsApp.java
@@ -2,8 +2,10 @@ package com.informatorio.tpfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.informatorio.tpfinal.entity")  // where entities are located.
 public class NewsApp {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/informatorio/tpfinal/controller/AuthorController.java
@@ -0,0 +1,4 @@
+package com.informatorio.tpfinal.controller;
+
+public class AuthorController {
+}
