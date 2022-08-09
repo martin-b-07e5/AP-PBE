0124e3d23d1f24247208a944565da5ed02c3815f
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-09 20:11:38 -0300

    like select * from authors

--- src/main/java/com/informatorio/tpfinal/controller/AuthorController.java
+++ src/main/java/com/informatorio/tpfinal/controller/AuthorController.java
@@ -1,14 +1,30 @@
 package com.informatorio.tpfinal.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.informatorio.tpfinal.entity.Author;
+import com.informatorio.tpfinal.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController  //https://www.baeldung.com/spring-controller-vs-restcontroller
-@RequestMapping("/author")  //https://www.baeldung.com/spring-requestmapping
+//@RequestMapping("/author")  //https://www.baeldung.com/spring-requestmapping
 public class AuthorController {
+
+    @Autowired
+    private AuthorRepository repository;
+
+    //    private HelloService helloService = new HelloService();
+    /*  Aplico el concepto de INYECCIÓN DE DEPENDENCIA,
+      no lo instancio, lo delego.
+      Ya no tengo el control, invierto el control (con @Autowired)
+      y el que tiene el control es SPRING */
+
     @GetMapping("/")
     public String hello() {
         return "HELLO from the controller";
+    }
+
+    @RequestMapping(value = "/author", method = RequestMethod.GET)
+    public @ResponseBody Iterable<Author> findEmpleados() {
+        return repository.findAll();
     }
 }
