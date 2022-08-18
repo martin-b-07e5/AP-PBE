68de4d594177dad9543ebebfe6a5c6b10f1deed9
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-18 08:58:41 -0300

    Optional: The query should also cover the content and author fullname

--- src/main/java/com/informatorio/tpfinal/repository/ArticleRepository.java
+++ src/main/java/com/informatorio/tpfinal/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.informatorio.tpfinal.repository;
 
 import com.informatorio.tpfinal.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,11 @@ import java.util.List;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByTitleContainingAndDescriptionContaining(String title, String description);
+
+    @Query(value = "SELECT articles.title as TITLE, articles.description AS DESCRIPTION, articles.content AS CONTENT, authors.full_name AS \"FULL NAME\"\n" +
+            "FROM articles\n" +
+            "INNER JOIN authors\n" +
+            "on articles.id_author = authors.id_author;\n", nativeQuery = true)
+    List<Article> getValues(String title, String description, String content, String fullName);
+
 }
