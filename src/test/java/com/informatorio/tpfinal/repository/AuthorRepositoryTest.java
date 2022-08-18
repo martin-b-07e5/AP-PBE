bfcb33fe81c4911aff42f8650bb55dc3a2652c6e
martin-b-07e5 <martin.bergagno@gmail.com>
2022-08-18 16:39:36 -0300

    starting test

---
+++ src/test/java/com/informatorio/tpfinal/repository/AuthorRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.informatorio.tpfinal.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorRepositoryTest {
+
+    @Test
+    void findByFullNameContaining() {
+    }
+
+    @Test
+    void findByCreatedAtGreaterThanEqual() {
+    }
+}
