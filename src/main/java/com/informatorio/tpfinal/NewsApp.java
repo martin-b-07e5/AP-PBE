78f4a4f2948ea915fdc65dd0bef4587e5f9e9dbb
martin-b-07e5 <martin.bergagno@gmail.com>
2022-12-21 23:47:55 -0300

    comment added

--- src/main/java/com/informatorio/tpfinal/NewsApp.java
+++ src/main/java/com/informatorio/tpfinal/NewsApp.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/* https://www.baeldung.com/spring-autowire
+Moreover, Spring Boot introduces the @SpringBootApplication annotation.
+This single annotation is equivalent to using @Configuration, @EnableAutoConfiguration, and @ComponentScan.
+Let's use this annotation in the main class of the application:
+*/
 @SpringBootApplication
 // https://www.baeldung.com/spring-entityscan-vs-componentscan
 @EntityScan("com.informatorio.tpfinal.entity")  // package where entities are located.
