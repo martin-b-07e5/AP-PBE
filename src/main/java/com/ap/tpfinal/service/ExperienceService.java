e3b8b630c302b3217fdad897ef9353e8bcc93597
martin-b-07e5 <martin.bergagno@gmail.com>
2023-01-06 07:57:18 -0300

    added existById and existsByName on ExperienceService.java

--- src/main/java/com/ap/tpfinal/service/ExperienceService.java
+++ src/main/java/com/ap/tpfinal/service/ExperienceService.java
@@ -5,16 +5,19 @@ import com.ap.tpfinal.repository.ExperienceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+//@Transactional
 public class ExperienceService {
 
     @Autowired
     private ExperienceRepository experienceRepository;
 
     //  Below functions are called from the controller
-    //    ------------------------
+
+    // READ ------------------------
     public List<Experience> getAll() {
         return experienceRepository.findAll();
     }
@@ -23,21 +26,23 @@ public class ExperienceService {
         return experienceRepository.findById(id).orElse(null);
     }
 
-    //    ------------------------
+    // CREATE ------------------------
     public Experience add(Experience experience) {
         return experienceRepository.save(experience);
     }
 
+    // UPDATE ------------------------
     public Experience update(Experience experience) {
         return experienceRepository.save(experience);
     }
 
+    // DELETE ------------------------
     public String deleteById(Long id) {
         experienceRepository.deleteById(id);
         return "experience " + id + " deleted";
     }
 
-    //    ------------------------
+    // ------------------------
     public List<Experience> findByNameContaining(String name) {
         int query = name.length();
         if (query >= 3) {
@@ -47,6 +52,15 @@ public class ExperienceService {
                     "The query must be 3 characters or more: " +
                             "Name: " + name
             );
+    }
+
+    // ------------------------
+    public boolean existById(Long id) {
+        return experienceRepository.existsById(id);
+    }
+
+    public boolean existByName(String name) {
+        return experienceRepository.existsByName(name);
     }
 
 }
