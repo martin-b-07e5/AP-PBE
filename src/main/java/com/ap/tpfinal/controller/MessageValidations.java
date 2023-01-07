ee580b088e2787170d0787a34a8e7f2c88721f8b
martin-b-07e5 <martin.bergagno@gmail.com>
2023-01-07 09:31:10 -0300

    .

---
+++ src/main/java/com/ap/tpfinal/controller/MessageValidations.java
@@ -0,0 +1,26 @@
+package com.ap.tpfinal.controller;
+
+public class MessageValidations {
+
+    private String mensaje;
+
+
+    // constructors
+    public MessageValidations() {
+    }
+
+    public MessageValidations(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+
+    // getters-setters
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
