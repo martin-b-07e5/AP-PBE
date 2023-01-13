5f06456a9183d87f4ad295ca10433c6429a5a78d
martin-b-07e5 <martin.bergagno@gmail.com>
2023-01-13 17:33:56 -0300

    .

--- src/main/java/com/ap/tpfinal/controller/TechnologyController.java
+++ src/main/java/com/ap/tpfinal/controller/TechnologyController.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 @RestController  //https://www.baeldung.com/spring-controller-vs-restcontroller
 @RequestMapping(value = "/api/v1/technology")  //https://www.baeldung.com/spring-requestmapping
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://ap-front--2023-01-12--firebase.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "https://ap-front--2023-01-12--firebase.web.app")
 public class TechnologyController {
 
     @Autowired
